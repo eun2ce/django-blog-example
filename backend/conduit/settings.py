d973f6ef3f2d06e14e2ec057270299de809c937d
eun2ce <joeun2ce@gmail.com>
2021-09-26 04:30:50 +0900

    update

--- backend/conduit/settings.py
+++ backend/conduit/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2^f+3@v7$v1f8yt0!s)3-1t$)tlp+xm17=*g))_xoi&&9m#2a&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -101,7 +101,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
@@ -152,6 +152,7 @@ CORS_ORIGIN_WHITELIST = (
     '0.0.0.0:4000',
     'http://localhost:4000',
     'http://127.0.0.1:8000',
+    'http://0.0.0.0:8000',
 )
 
 # Tell Django about the custom `User` model we created. The string
--- frontend/src/agent.js
+++ frontend/src/agent.js
@@ -6,7 +6,7 @@ import authStore from './stores/authStore';
 const superagent = superagentPromise(_superagent, global.Promise);
 
 //const API_ROOT = 'https://conduit.productionready.io/api';
-const API_ROOT = 'http://127.0.0.1:8000/api';
+const API_ROOT = 'http://0.0.0.0:8000/api';
 
 const encode = encodeURIComponent;
 
